1eaa58b937a6851c704db318b9c8acf64f0338fd
bluewins <bluewins84@naver.com>
2020-02-18 15:12:44 +0900

    zabbix-install.sh

---
+++ zabbix-install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Zabbix Install Script
+# https://repo.zabbix.com/zabbix/
+# 공인 연결이 되어 있을 경우 사용
+
+OS=`uname -r`
+HOSTNAME=`hostname`
+ZIP='10.251.0.87'
+
+if [[ "$OS" == *"el6"* ]];then
+    wget https://repo.zabbix.com/zabbix/4.0/rhel/6/x86_64/zabbix-agent-4.0.15-1.el6.x86_64.rpm
+    rpm -ivh zabbix-agent-4.0.15-1.el6.x86_64.rpm
+    sed -i "98s/.*/Server=$ZIP/g" /etc/zabbix/zabbix_agentd.conf
+    sed -i "139s/.*/ServerActive=$ZIP:10051/g" /etc/zabbix/zabbix_agentd.conf
+    sed -i "150s/.*/Hostname=$HOSTNAME/g" /etc/zabbix/zabbix_agentd.conf
+    chkconfig zabbix-agent on
+    service zabbix-agent restart
+elif [[ "$OS" == *"el7"* ]];then
+    wget https://repo.zabbix.com/zabbix/4.0/rhel/7/x86_64/zabbix-agent-4.0.15-1.el7.x86_64.rpm
+    rpm -ivh zabbix-agent-4.0.15-1.el7.x86_64.rpm
+    sed -i "98s/.*/Server=$ZIP/g" /etc/zabbix/zabbix_agentd.conf
+    sed -i "139s/.*/ServerActive=$ZIP:10051/g" /etc/zabbix/zabbix_agentd.conf
+    sed -i "150s/.*/Hostname=$HOSTNAME/g" /etc/zabbix/zabbix_agentd.conf
+    systemctl start zabbix-agent.service
+    systemctl enable zabbix-agent.service
+fi
